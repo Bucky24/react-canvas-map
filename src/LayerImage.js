33ee6c794f98869c1f32c61f03043682d3885c39
Robbert Wijtman <robbert@xinxmedia.com>
2024-04-26 08:57:04 -0700

    handle xoff and yoff in LayerImage

--- src/LayerImage.js
+++ src/LayerImage.js
@@ -4,9 +4,9 @@ import { Image } from '@bucky24/react-canvas';
 import { Cell }  from './Cell';
 
 export function LayerImage(props) {
-    const { x, y, width, height, ...rest } = props;
+    const { x, y, width, height, xOff, yOff, ...rest } = props;
 
-    return <Cell x={x} y={y} height={height} width={width}
+    return <Cell x={x+ (xOff ?? 0)} y={y + (yOff ?? 0)} height={height} width={width}
         cb={useCallback((dims) => {
             return <Image {...dims} {...rest} />
         },[rest])}
